1395bb75eea409b0d0766826397d88846aa24d19
Enock <enock.mokua@student.moringaschool.com>
2023-01-11 23:58:52 +0300

    generate access token request

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -1,6 +1,28 @@
 class MpesasController < ApplicationController
+  require "rest-client"
 
   private
 
+  def generate_access_token_request
+    @url =
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
+    @consumer_key = ENV["MPESA_CONSUMER_KEY"]
+    @consumer_secret = ENV["MPESA_CONSUMER_SECRET"]
+    @userpass = Base64.strict_encode64(`#{@consumer_key}:#{@consumer_secret}`)
+    @headers = { Authorization: "Bearer#{@userpass}" }
+    res =
+      RestClient::Request.execute(
+        url: @url,
+        method: :get,
+        headers: "Basic #{@userpass}"
+      )
+
+    res
+  end
+
+  def get_access_token
+
+    
+  end
 end
